c9788a6473e05d64d787b7118d8e73bccc5832e4
Pradeep0911-S <sarangapradeep26@gmail.com>
2025-03-13 12:19:34 +0530

    Add files via upload

--- LAB4/l4switchdemo.dart
+++ LAB4/l4switchdemo.dart
@@ -1,12 +1,18 @@
-import 'package:l4switchdemo/l4switchdemo.dart' as l4switchdemo;
-import 'package:l4switchdemo/switchdemo.dart';
-import 'dart:io';
-
-void main(List<String> arguments) {
-	String c = stdin.readLineSync()!;
-	print(l4switchdemo.sdemo(c));
-	print(l4switchdemo.csdemo(c));
-	switchdemo sd = switchdemo();
-	print(sd.colors(c));
+String sdemo(String c){
+	var x = switch(c){
+		'red' || 'Red' || 'RED' => 'Red',
+		'yellow' || 'Yellow'  || 'YELLOW'=> 'Yellow',
+		'black' || 'Black'  || 'BLACK'=> 'Black',
+		_ => 'Nothing',
+	};
+	return x;
+}
+int csdemo(String c){
+	return switch(c){
+		'red' || 'Red' || 'RED' => 1 ,
+		'yellow' || 'Yellow'  || 'YELLOW'=> 2,
+		'black' || 'Black'  || 'BLACK'=> 3,
+		_ => 0,	
+	};
 
 }
