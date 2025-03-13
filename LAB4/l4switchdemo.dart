b0065667ae3d6874697a61081beb74bd65d0214d
Pradeep0911-S <sarangapradeep26@gmail.com>
2025-03-13 12:24:36 +0530

    Update l4switchdemo.dart

--- LAB4/l4switchdemo.dart
+++ LAB4/l4switchdemo.dart
@@ -1,3 +1,16 @@
+import 'package:l4switchdemo/l4switchdemo.dart' as l4switchdemo;
+import 'package:l4switchdemo/switchdemo.dart';
+import 'dart:io';
+
+void main(List<String> arguments) {
+	String c = stdin.readLineSync()!;
+	print(l4switchdemo.sdemo(c));
+	print(l4switchdemo.csdemo(c));
+	switchdemo sd = switchdemo();
+	print(sd.colors(c));
+
+}
+
 String sdemo(String c){
 	var x = switch(c){
 		'red' || 'Red' || 'RED' => 'Red',
